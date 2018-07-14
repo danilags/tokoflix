0a3b26bc540a7cc34e36ff585d4ca4191210c4ca
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-14 21:15:29 +0700

    minor css for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,17 +10,27 @@ import {
 } from 'reactstrap';
 
 const NavBar = (props) => (
-  <Navbar color="light" light expand="md">
-    <NavbarBrand href="/">TokoFlix</NavbarBrand>
+  <Navbar style={styles.container} light expand="md">
+    <NavbarBrand style={{ color: '#fff' }} href="/">TokoFlix</NavbarBrand>
     <NavbarToggler onClick={() => null } />
     <Collapse isOpen={false} navbar>
       <Nav className="ml-auto" navbar>
         <NavItem>
-          <NavLink href="https://github.com/danilags">Daniel Agus</NavLink>
+          <NavLink style={{ color: '#fff' }} href="https://github.com/danilags">Daniel Agus</NavLink>
         </NavItem>
       </Nav>
     </Collapse>
   </Navbar>
 );
+
+const styles = {
+  container: {
+    background: '#42b549', 
+    position: 'fixed', 
+    top: 0,
+    zIndex: 20,
+    width: '100%'
+  }
+}
 
 export default NavBar;
