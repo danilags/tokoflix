6eea3e0c5753de0c02a5202ca2628e403ef677bb
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-14 16:40:13 +0700

    calling action dispacher api on component home

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux'; 
+import {
+  Row,
+  Col
+} from 'reactstrap';
 
-import { Wrapper } from '../../components';
+import { Wrapper, FilmCard } from '../../components';
+import { getApiData } from '../../actions';
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      page: 1
     }
   }
 
   componentDidMount() {
-    
+    this.props.getApiData({ page: this.state.page })
   }
 
+  slugify(title) {
+    return title.split(' ').join('-').toLowerCase();
+  }
+
+  renderFilm() {
+    const { filmList } = this.props.films;
+    if (filmList.isFetch) {
+      return <h4>Loading...</h4>
+    }
+    return filmList.data.map((item, index) => {
+      
+      return <Col key={index} md={4}><a href={`/${item.id}/${this.slugify(item.title)}`}><FilmCard film={item} /></a></Col>
+    });
+  }
 
   render() {
-    console.log('REDUX', this.props);
     return (
       <Wrapper>
-       <h3>Home Page</h3>
+        <h4>Newly created movie</h4>
+       <Row>
+        { this.renderFilm() }
+       </Row>
       </Wrapper>
     )
   }
@@ -30,7 +51,11 @@ const mapStateToProps = state => ({
   films: state.filmReducer
 });
 
+const mapDispatchToProps = dispatch => ({
+  getApiData: (params) => dispatch(getApiData(params))
+})
 
-export default connect(mapStateToProps, null)(HomePage);
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
 
 
