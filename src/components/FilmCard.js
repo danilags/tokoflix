6eab4421eb813c4c08992a7840c10f7c67d2cb92
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-14 21:15:14 +0700

    adding reusable FilmCard

---
+++ src/components/FilmCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { IMAGE_BASE_URL } from '../constants';
+
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button
+} from 'reactstrap';
+
+const FilmCard = (props) => (
+  <Card style={styles.container}>
+    <CardBody>
+      <img style={{ width: '100%'  }} src={`${IMAGE_BASE_URL}${props.film.poster_path}`} alt="Card image cap" />
+    </CardBody>
+    <CardBody>
+      <CardTitle>{props.film.title}</CardTitle>
+      <CardTitle>Card subtitle</CardTitle>
+      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+    </CardBody>
+  </Card>
+)
+const styles = {
+  container: {
+    margin: '2rem 0',
+  }
+}
+
+export default FilmCard;
