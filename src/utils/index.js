a29edabd7f6d88e05246b1cbbcb8462f56185474
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-14 21:18:38 +0700

    adding function to convert price and convert to rupiah

--- src/utils/index.js
+++ src/utils/index.js
@@ -36,4 +36,25 @@ const converDate = (params) => {
   return `${date} ${monthNames[month]} ${year}`;
 }
 
-export { API_CALL, converDate };
+const convertPrice = (rating) => {
+ if (rating === 1 && rating <= 3) {
+    return 3500;
+ } else if (rating >= 3 && rating <= 6) {
+    return 8250;
+ } else if (rating >= 6 && rating <= 8) {
+    return 16350;
+ } else if (rating >= 8 && rating <= 10) {
+    return 21250;
+ } 
+}
+
+const convertToRupiah = (angka) => {
+  var rupiah = '';
+  var angkarev = angka.toString().split('').reverse().join('');
+  for (var i = 0; i < angkarev.length; i++)
+    if (i % 3 === 0)
+      rupiah += angkarev.substr(i, 3) + '.';
+  return rupiah.split('', rupiah.length - 1).reverse().join('');
+}
+
+export { API_CALL, converDate, convertPrice, convertToRupiah };
