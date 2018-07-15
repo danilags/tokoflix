d31bdd7a16f47681c5c0e1639c5f802c7979060a
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-15 23:37:33 +0700

    adding some validation check on modal text

--- src/components/ModalText.js
+++ src/components/ModalText.js
@@ -14,18 +14,23 @@ const ModalText = (props) => (
   <Modal isOpen={props.isOpen} toggle={props.onToggle}>
     <ModalHeader toggle={props.toggle}>Guest</ModalHeader>
       <ModalBody>
-        <Form onSubmit={props.onSubmitData}>
-          <FormGroup>
-            <Label for="exampleEmail">Name</Label>
-            <Input 
-              onChange={props.onChangesName}
-              type="text" 
-              name="name" 
-              placeholder="Your name"
-            />
-          </FormGroup>
-          <Button>Masuk</Button>
-        </Form>
+        {
+          props.isLogged ? <Button onClick={() => props.onLogout()}>Keluar</Button> :
+          <Form onSubmit={props.onSubmitData}>
+            <FormGroup>
+              <Label for="exampleEmail">Name</Label>
+              <Input 
+                onChange={props.onChangesName}
+                type="text" 
+                name="name" 
+                placeholder="Your name"
+              />
+            </FormGroup>
+            <Button disabled={props.isLoading}>
+              {props.isLoading ? 'Loading...' : 'Masuk'}
+            </Button>
+          </Form>
+        }
       </ModalBody>
   </Modal>
 )
